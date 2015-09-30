7283fa7a315ee725e6ad5e4423f977c5196957bd
Michael B. <Tankobot@users.noreply.github.com>
2015-09-30 07:40:26 -0700

    Update net.lua

--- lib/net.lua
+++ lib/net.lua
@@ -2,7 +2,15 @@
 
 local crypt = {}
 
-function crypt.keyGen(length)
+local function hex(num)
+	return string.format("%X", num)
+end
+
+function crypt.hash(str)
+	
+end
+
+function crypt.keyGen(seed)
 	local key = ""
 	local function randomHex(prev)
 		local ran = math.random(1, 15)
@@ -16,32 +24,4 @@ function crypt.keyGen(length)
 		key = key..randomHex()
 	end
 	return key
-end
-function crypt.crypt(str, key)
-	assert(#key%2==0, "Incorrect key length.")
-	local hex = {
-		A = 10, B = 11, C = 12,
-		D = 13, E = 14, F = 15,
-	}
-	--Setup key table
-	local keyTab = {}
-	for i=1, #key do
-		if tonumber(key:sub(i, i)) then
-			table.insert(keyTab, tonumber(key:sub(i, i)))
-		else
-			table.insert(keyTab, hex[key:sub(i, i)])
-		end
-	end
-	--Byte rounds
-	for i=1, #str do
-		local strByte = str:sub(i, i)
-		local keyByte = key:sub(i, i)
-		--Hex rounds
-		for i=1, 2 do
-			--Bit rounds
-			for i=1, 4 do
-				
-			end
-		end
-	end
 end
