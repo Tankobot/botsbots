e72ea71830250df6232e78c179f74cca608a1053
Michael B <Tankobot@users.noreply.github.com>
2015-12-11 13:32:08 -0800

    Update cog.lua

--- lib/cog.lua
+++ lib/cog.lua
@@ -1,4 +1,11 @@
 --[[
 Name: Cog Graphical User Interface 
 Outline: 
+	Declare global variable 'cog'
+	Store private frame table 
+	Declare methods 
+		Storing new frames 
+		Create new objects 
+		Pull cog event 
+		Yield to cog interface 
 --]]
